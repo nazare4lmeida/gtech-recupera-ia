3ba3586477161ad416ea88ac3bb32a123ab7b799
na4lmeida <nazyalmeida@gmail.com>
2026-04-04 14:09:50 -0300

    corrigido horário

--- client/src/utils/specialModules.ts
+++ client/src/utils/specialModules.ts
@@ -1,7 +1,7 @@
 import { COURSE_CONTENT } from "../data/courseContent";
 import { CourseTrack } from "../types";
 
-export const WINDOW_OPEN = new Date("2026-04-04T13:00:00").getTime();
+export const WINDOW_OPEN = new Date("2026-04-04T17:00:00").getTime();
 export const WINDOW_CLOSE = new Date("2026-04-11T17:00:00").getTime();
 export const RECOVERY_PASSING_SCORE = 6;
 export const RECOVERY_QUESTIONS = {
